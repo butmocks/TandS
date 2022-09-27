831968b5e62462d99c7c33d4e80af3f5bde6842b
Yaroslav Platonov <butmocks@gmail.com>
2022-09-27 19:38:42 +0300

    buttons and gameselection

--- routes/src/GameType.jsx
+++ routes/src/GameType.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 const GameType = () => {
+  const gameTypeSelect = ['white', 'yellow', 'pink', 'red'];
+  const [level, setLevel] = useState('white');
+  const handleChange = event => {
+    setLevel(event.target.value);
+  };
+
+
   return (
     <div className="page__content">
       <h1>Виберіть режим гри</h1>
       <p>рівень розкутості</p>
-<label>
-  Оберіть колір складності, згідно якому змінюватимуться завдання
-       <select>рівень</select>
-        {/* <select value={} onChange={}></select> */}
-</label>
-      <Link to="/">головна</Link>
+
+
+      <label>
+        Режим
+        <select value={level} onChange={handleChange}>
+          {gameTypeSelect.map(option => (
+            <option setValue={option}>{option}</option>
+          ))}
+        </select>
+      </label>
+
+
+      {/* 
+      <label>
+        Оберіть колір складності, згідно якому змінюватимуться завдання
+        <select>рівень</select>
+        <select value={} onChange={}></select>
+      </label> */}
+
+
+      <button>
+        <Link to="/start">Почати</Link>
+
+      </button>
+      <button>
+        <Link to="/">головна</Link>
+      </button>
+      
     </div>
   );
 };
--- routes/src/Home.jsx
+++ routes/src/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
   return (
     <div className="page__content">
       <h1>Тут будуть інпути імен</h1>
+      <Link to="/gametype">Обрати режим гри</Link>
       <Link to="/start">Почати гру</Link>
     </div>
   );
